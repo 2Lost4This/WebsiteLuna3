90826cced4e7199a6e4d09663cebd15ef4b14b4d
2Lost4GitHub <91763324+2Lost4This@users.noreply.github.com>
2022-01-29 19:57:05 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,7 @@ export default function render({ Component, pageProps }) {
                 <Layout />
                 <div className='set-luna'>
                     <Link href='https://waya.one/go/luna'>
-                        <a>Creditsh&nbsp;<AiFillHeart style={{ position: 'relative', top: 2 }} />&nbsp;to&nbsp;Luna</a>
+                        <a>Credit&nbsp;<AiFillHeart style={{ position: 'relative', top: 2 }} />&nbsp;to&nbsp;Luna</a>
                     </Link>
                 </div>
                 <Component {...pageProps} />
