af80157446479cff2e61c03d11e2e146dd5f6144
2Lost4GitHub <91763324+2Lost4This@users.noreply.github.com>
2022-01-30 14:33:31 -0500

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -6,11 +6,11 @@ export default class Error500 extends React.Component {
         return (
             <>
                 <Head>
-                    <title>Luna - 500</title>
+                    <title>2Lost4Discord - 500</title>
                 </Head>
                 <div className={style.align}>
                     <div className={style.container}>
-                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>500</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}> ⌋ Internal server error</text> <br />
+                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>500</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}> ⌋ Ohh this is awkward, looks like we have an Internal server error</text> <br />
                     </div>
                 </div>
             </>
