27b8aef72426a00e424c25a4fe850efcb207ae0d
Luni_sia <luni.siaa@gmail.com>
2022-01-30 01:05:36 +0100

    update tags

--- pages/_app.js
+++ pages/_app.js
@@ -16,28 +16,26 @@ export default function render({ Component, pageProps }) {
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
 
-                <meta property='title' content='Luna - Software Engineer and LGBTQ+ Activist' />
-                <meta property='description' content="Heyia, I'm Luna Seemann and I was born 14 years ago and live in Austria." />
-                <meta name='keywords' content='Luna, Lunish, Lunish#8888, waya, waya.one, waya profiles, Luna profile, Luna onlyfans, Luna github, Lunish onlyfans, Lunish githu, Luna Seemann, Seemann, discord-botlist.eu, Flamex, Bendix, dbleu' />
+                <meta property='title' content='2Lost4Discоrd' />
+                <meta property='description' content="I am 2Lost4Discord or for short, Losti I like skateboarding and mountain biking. I also like to meet friends and spend time on Discord. I also program a little bit in my free time Learn more about my story and my projects" />
 
-                <meta name="twitter:site" content="@waya.one" />
-                <meta name="twitter:title" content="Luna - Software Engineer and LGBTQ+ Activist" />
-                <meta name="twitter:description" content="Heyia, I'm Luna Seemann and I was born 14 years ago and live in Austria." />
-                <meta name="twitter:image" content="https://avatars.githubusercontent.com/u/71079641?v=4" />
+                <meta name="twitter:title" content="2Lost4Discоrd" />
+                <meta name="twitter:description" content="I am 2Lost4Discord or for short, Losti I like skateboarding and mountain biking. I also like to meet friends and spend time on Discord. I also program a little bit in my free time Learn more about my story and my projects" />
+                <meta name="twitter:image" content="https://cdn.waya.one/r/e4ad8f5b91253285ab8e592f7dcad965.png" />
 
-                <meta property='og:title' content='Luna - Software Engineer and LGBTQ+ Activist' />
-                <meta property='og:description' content="Heyia, I'm Luna Seemann and I was born 14 years ago and live in Austria." />
+                <meta property='og:title' content='2Lost4Discоrd' />
+                <meta property='og:description' content="I am 2Lost4Discord or for short, Losti I like skateboarding and mountain biking. I also like to meet friends and spend time on Discord. I also program a little bit in my free time Learn more about my story and my projects" />
                 <meta property='og:type' content='website' />
-                <meta property='og:image' content='https://avatars.githubusercontent.com/u/71079641?v=4' />
+                <meta property='og:image' content='https://cdn.waya.one/r/e4ad8f5b91253285ab8e592f7dcad965.png' />
 
                 <meta name='theme-color' content='#f19bec' />
-                <title> Luna - Software Engineer and LGBTQ+ Activist</title>
-                <link rel='shortcut icon' type='image/x-icon' href='https://avatars.githubusercontent.com/u/71079641?v=4' />
+                <title>2Lost4Discоrd</title>
+                <link rel='shortcut icon' type='image/x-icon' href='https://cdn.waya.one/r/e4ad8f5b91253285ab8e592f7dcad965.png' />
             </Head>
             <body>
                 <Layout />
                 <div className='set-luna'>
-                    <Link href='/go/luna'>
+                    <Link href='https://waya.one/go/luna'>
                         <a>Made with&nbsp;<AiFillHeart style={{ position: 'relative', top: 2 }} />&nbsp;by&nbsp;Luna</a>
                     </Link>
                 </div>
