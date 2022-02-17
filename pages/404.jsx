2b5ccda61bbb1472126524f29925729c02be3042
2Lost4GitHub <91763324+2Lost4This@users.noreply.github.com>
2022-02-17 13:15:25 -0500

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,21 +1,17 @@
 import style from '../styles/Error.module.css';
 import React from 'react';
 import Head from 'next/head';
-
-import { HiHome } from 'react-icons/hi';
-export default class Error404 extends React.Component {
+export default class Error500 extends React.Component {
     render() {
         return (
             <>
                 <Head>
-                    <title>2Lost4Discord - 404</title>
+                    <title>2Lost4Discord - 500</title>
                 </Head>
                 <div className={style.align}>
                     <div className={style.container}>
-                        <div className={style.align}>                     
-                            <div className={style.container}> 
-                            <img src='https://media.discordapp.net/attachments/883817635081506886/943903972241055854/404.jpeg' style={{ width: '100%' }} />
-                        </div>
+                        <img src='https://media.discordapp.net/attachments/883817635081506886/943903972241055854/404.jpeg' style={{ width: '100%' }} />
+                    </div>
                 </div>
             </>
         );
